c3991bbd0f7ef500c7a23edd74a441e59a07ceab
QiongLuoJQ <Meiyangqiong@outlook.com>
2024-11-23 22:53:48 +0800

    Add files via upload

---
+++ SEER_3_3res_lasso.R
@@ -0,0 +1,144 @@
+rm(list=ls())
+### 直接使用需要的模型，按照默认的超参数进行模型训练。----
+library(mlr3verse) # 0.2.8
+library(mlr3proba) # 0.6.0
+library(mlr3tuning) #0.20.0
+library(mlr3filters) #0.7.1
+library(tidyverse) # 2.0.0
+## mlr3 0.18.0 
+## mlr3extralearners 0.7.1-9000
+list1 = c("1base_spm","2base_tnm","3base_spm_tnm","4all")
+
+for(i in 1:4){
+  
+  file1 = paste0("./SEER/lung/1data/SEER_lung_tasks_lasso",list1[i],".rdata")
+  load(file1)
+  
+  task_f <- function(taskdata, task_name) {
+    task <- as_task_surv(taskdata,
+                         time = "Survival_time",
+                         event = "Status",
+                         id = task_name)
+    return(task)
+  }
+  lasso_tasks_train <- task_f(lasso_tasks_train$data(), "lasso_train")
+  lasso_tasks_test <- task_f(lasso_tasks_test$data(), "lasso_test")
+  
+  # 加速
+  wks = 4
+  library(future)
+  plan("multisession",workers = wks)
+  
+  library(paradox)
+  # 创建一个预处理操作
+  task_scale <- po("scale", center = T, scale = TRUE)
+  # 使用预处理操作拟合训练任务并返回预处理后的任务
+  train_task <- task_scale$train(list(lasso_tasks_train))[[1]]
+  # 使用相同的预处理操作转换测试任务
+  test_task <- task_scale$predict(list(lasso_tasks_test))[[1]]
+  ## 这里需要注意的是，应该用predict 才能将train的均数标准差移植到test
+  ## 如果test还是用train进行标准化，那就是test自己的，此时就会出现范围错位。
+  # test_task_1 <- task_scale$train(list(lasso_tasks_test))[[1]]
+  
+  ###测试模型显示以下模型可以纳入。
+  surv_list = c("surv.glmnet","surv.blackboost",
+                "surv.coxboost",
+                "surv.rfsrc","surv.ctree",
+                "surv.xgboost.cox","surv.gbm",
+                "surv.coxph")
+  
+  surv_lrns = lrns(surv_list)
+  
+  lrns_all = surv_lrns
+  for(i in 2:length(surv_lrns)){
+    lrns_all[[i]]$param_set$values <- surv_lrns_tuning[[i-1]]$param_set$values
+  }
+  
+  library(survival)
+  library(survminer)
+  library(survex)
+  
+  set.seed(180615)
+  surv_total <- vector("list")
+  surv_exp <- vector("list")
+  msr_surv <- c("surv.cindex","surv.graf")
+  
+  for(k in 1:length(surv_list)){
+    print(k)
+    surv_lrn = surv_lrns[[k]]
+    surv_lrn$id
+    
+    if(surv_lrns[[k]]$id == "surv.rfsrc" | 
+       surv_lrns[[k]]$id == "surv.ctree" |
+       surv_lrns[[k]]$id == "surv.xgboost.cox" | 
+       surv_lrns[[k]]$id == "surv.gbm"){
+      surv_lrn <- as_learner(ppl(
+        "distrcompositor",
+        learner = surv_lrn,
+        estimator = "kaplan",
+        form = "ph"
+      ))
+    } 
+    
+    set.seed(180615)
+    surv_lrn$train(train_task)
+    print(surv_lrn)
+    # important!
+    class(surv_lrn) <- c(class(surv_lrn), "LearnerSurv") 
+    
+    msr_surv <- c("surv.cindex","surv.graf")
+    
+    surv_pre = surv_lrn$predict(test_task)
+    surv_pre
+    
+    # lp = predict(surv_lrn, newdata = test_task$data())
+    
+    surv_res = surv_pre$score(msrs(msr_surv)) %>% round(4)
+    print(surv_res)
+    
+    # surv_pre$score(msr("surv.uno_auc"), train_set = train_task,
+    #                task = test_task) %>% round(4)
+    
+    set.seed(180615)
+    surv_exp_test <- explain(surv_lrn,
+                             data = test_task$data(),
+                             y = Surv(test_task$data()$Survival_time,
+                                      test_task$data()$Status),
+                             label = surv_lrns[[k]]$id)
+    # 计算C-inde
+    cindex = c_index(y_true = surv_exp_test$y,
+                     risk = surv_exp_test$predict_function(surv_exp_test$model,
+                                                           surv_exp_test$data))
+    print(cindex)
+    # 计算integrated Brier score
+    surv = surv_exp_test$predict_survival_function(surv_lrn,
+                                                   surv_exp_test$data,
+                                                   surv_exp_test$times)
+    ibs = integrated_brier_score(surv_exp_test$y,
+                                 surv = surv,
+                                 times = surv_exp_test$times)
+    # 计算integrated AUC
+    auc = integrated_cd_auc(surv_exp_test$y,
+                            surv = surv,
+                            times = surv_exp_test$times)
+    print(ibs)
+    print(auc)
+    
+    surv_res_all <- c(surv_res, cindex = cindex, ibs = ibs, iauc = auc ) %>%
+      round(4)
+    
+    surv_total[[surv_lrn$id]] = c(lrn = surv_lrn$id, 
+                                  surv_res_all)
+    surv_exp[[surv_lrn$id]] = surv_exp_test
+  }
+  
+  surv_df = do.call(rbind,surv_total) %>% as.data.frame() 
+  
+  file1 <- "./SEER/lung/3result/03surv_res_lasso/"
+  if(!file.exists(file1)){
+    dir.create(file1)
+  }
+  save(surv_total, file = paste0(file1, list1[i],"_surv_res_lasso.rdata"))
+  save(surv_exp, file = paste0(file1, list1[i],"_surv_exp_lasso.rdata"))
+  openxlsx::write.xlsx(surv_df, file = paste0(file1, list1[i],"_surv_res_list_lasso.xlsx"))
+}
